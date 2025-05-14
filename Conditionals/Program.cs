4797f9f87e7d9991e51b9663424c856831a47ef1
ilyaskoccc <ilyaskoc626@gmail.com>
2025-05-14 17:37:58 +0300

    Switch-Case

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -29,8 +29,38 @@ namespace Conditionals
 
             // Ternary operator
             Console.WriteLine(number == 0 ? "Number is zero" : number > 0 ? "Number is positive" : "Number is negative");
-       
-            Console.ReadLine();
+
+            // Switch statement
+            Console.WriteLine("--------------SWITCH-----------------");
+            var day = 3;
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Invalid day");
+                    break;
+                }
+                Console.ReadLine();
         }
     }
 }
