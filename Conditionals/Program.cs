d84c2feabaaa7af485e5365ffba4739fec9cb6c4
ilyaskoccc <ilyaskoc626@gmail.com>
2025-05-14 17:34:15 +0300

    IF-ELSE-ELSE IF

---
+++ Conditionals/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------------IF-ELSE-----------------");
+            var number = 10;
+            if (number > 0)
+            {
+                Console.WriteLine("Number is positive");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine("Number is negative");
+            }
+            else
+            {
+                Console.WriteLine("Number is zero");
+            }
+       
+            Console.ReadLine();
+        }
+    }
+}
