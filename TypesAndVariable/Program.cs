0008b2d92458a6b78e94ad95fa5b6b47049fc11c
ilyaskoccc <ilyaskoc626@gmail.com>
2025-05-14 15:54:40 +0300

    Created Value Type Example

--- TypesAndVariable/Program.cs
+++ TypesAndVariable/Program.cs
@@ -26,7 +26,43 @@ namespace TypesAndVariable
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine("-------------------------------");
+
+            //Value Types
+            int number1 = 2147483647;
+            long number2 = 9223372036854775807;
+            short number3 = 32767;
+            byte number4 = 255;
+            double number5 = 10.4;
+            decimal number6 = 10;
+            char character = 'A';
+            bool isTrue = true;
+
+            Console.WriteLine("Value Types:");
+            Console.WriteLine("int: number1 is{0}",number1);
+            Console.WriteLine("long: number2 is{0}", number2);
+            Console.WriteLine("short: number3 is{0}", number3);
+            Console.WriteLine("byte: number4 is{0}", number4);
+            Console.WriteLine("double: number5 is{0}", number5);
+            Console.WriteLine("decimal: number6 is{0}", number6);
+            Console.WriteLine("char: character is{0}", character);
+            Console.WriteLine("bool: isTrue is{0}", isTrue);
+            Console.WriteLine((int)Days.Friday);
+
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("Press any key to continue...");
             Console.ReadLine();
         }
+    }
+
+    enum Days
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
     }
 }
