3e564de746cf82aa00984e39899d26303fd66385
ilyaskoccc <ilyaskoc626@gmail.com>
2025-05-17 15:08:07 +0300

    Created Arrays and Example

---
+++ Arrays/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[5] {"İlyas", "Volkan", "Ahmet", "Ali", "Veli"};
+
+            Console.WriteLine("----------NAMES------------");
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine("----------REGIONS------------");
+
+            string[,] regions = new string[3, 3]
+            {
+                {"İstanbul", "Bursa", "İzmit"},
+                {"Ankara", "Konya", "Kırıkkale"},
+                {"İzmir", "Manisa", "Aydın"}
+            };
+
+            for (int i = 0; i <= regions.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= regions.GetUpperBound(1); j++)
+                {
+                    Console.WriteLine(regions[i, j]);
+                }
+                Console.WriteLine("***************");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
