bda5c93efc10eab6ab8c15595640f87badf241cd
ilyaskoccc <ilyaskoc626@gmail.com>
2025-05-17 15:19:22 +0300

    Created Loops Example

---
+++ Loops/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // For Loop
+            for (int i = 10; i >= 0; i = i - 2)
+            {
+                Console.WriteLine("For Loop: " + i);
+            }
+            Console.WriteLine("----------NEW-LINE------------");
+            // While Loop
+            int j = 1;
+            while (j <= 10)
+            {
+                Console.WriteLine("While Loop: " + j);
+                j = j + 2;
+            }
+            Console.WriteLine("----------NEW-LINE------------");
+            // Do While Loop
+            int k = 0;
+            do
+            {
+                Console.WriteLine("Do While Loop: " + k);
+                k++;
+            } while (k < 10);
+            Console.ReadLine();
+        }
+    }
+}
