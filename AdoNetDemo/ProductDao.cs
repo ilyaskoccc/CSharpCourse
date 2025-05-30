56a76c21812fe2cc6080a381c72e5168f2ff0542
ilyaskoccc <ilyaskoc626@gmail.com>
2025-05-30 15:13:53 +0300

    Created AdoNetDemo with GetAll Method and SQL Database Connection Example

---
+++ AdoNetDemo/ProductDao.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetDemo
+{
+    public class ProductDao
+    {
+        public List<Product> GetAll()
+        {
+            SqlConnection connection = new SqlConnection(@"server=(localdb)\mssqllocaldb; initial catalog=Etrade; integrated security=true");
+           
+            if (connection.State == ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+
+            SqlCommand command = new SqlCommand("SELECT * FROM Products", connection);
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            DataTable dataTable = new DataTable();
+            dataTable.Load(reader);
+            reader.Close();
+            connection.Close();
+            return dataTable.AsEnumerable().Select(row => new Product
+            {
+                Id = row.Field<int>("Id"),
+                Name = row.Field<string>("Name"),
+                UnitPrice = row.Field<decimal>("UnitPrice"),
+                StockAmount = row.Field<int>("StockAmount")
+            }).ToList();
+        }
+    }
+}
