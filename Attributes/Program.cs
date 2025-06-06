ced36a052d524bac91ce327ba0e2e425c862b4db
ilyaskoccc <ilyaskoc626@gmail.com>
2025-06-06 17:59:57 +0300

    Created Attributes Class And Example

---
+++ Attributes/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Attributes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Doe",
+                Age = 30
+            };
+
+            PersonDao personDao = new PersonDao();
+            personDao.Add(person); // This will show a warning because of the Obsolete attribute
+            personDao.AddNew(person); // This is the preferred method
+        }
+
+
+    }
+
+    [Table("Person")]
+    public class Person
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public int Age { get; set; }
+    }
+
+    public class PersonDao
+    {
+        [Obsolete("Use AddNew instead", false)]
+        public void Add(Person person)
+        {
+            Console.WriteLine($"Adding person: {person.FirstName} {person.LastName}, Age: {person.Age}");
+        }
+
+        public void AddNew(Person person)
+        {
+            Console.WriteLine($"Adding person: {person.FirstName} {person.LastName}, Age: {person.Age}");
+        }
+    }
+    [AttributeUsage(AttributeTargets.Property)]
+    class RequiredAttribute : Attribute
+    {
+    }
+
+    [AttributeUsage(AttributeTargets.Class)]
+    class TableAttribute : Attribute
+    {
+        public string Name { get; }
+        public TableAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}
