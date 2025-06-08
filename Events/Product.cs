14718d8603d61210c9aa1ac9fc5ec832103e7592
ilyaskoccc <ilyaskoc626@gmail.com>
2025-06-08 20:03:43 +0300

    Created Event Program And Product Class With Example

---
+++ Events/Product.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    public delegate void StockControl();
+    public class Product
+    {
+        private int _stock;
+
+        public Product(int stock)
+        {
+            _stock = stock;
+        }
+
+        public event StockControl StockControlEvent;
+        public string ProductName { get; set; }
+        public int Stock
+        {
+            get
+            {
+                return _stock;
+            }
+            set
+            {
+                _stock = value;
+                if (_stock <= 15 && StockControlEvent != null)
+                {
+                    StockControlEvent(); // Trigger the event if stock is below 10
+                }
+            }
+        }
+
+        public void Sell(int amount)
+        {
+            Stock -= amount;
+            Console.WriteLine("{0} Stock amount : {1}", ProductName, Stock);
+        }
+    }
+}
---
+++ Events/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product(50);
+            product.ProductName = "Harddisk";
+
+            Product product2 = new Product(50);
+            product2.ProductName = "Phone";
+            product2.StockControlEvent += product2_StockControlEvent;
+            //product.StockControlEvent += () => Console.WriteLine("Stock is low for {0}!", product.ProductName);
+
+            for (int i = 0; i < 5; i++)
+            {
+                product.Sell(10);
+                product2.Sell(10);
+                Console.ReadLine();
+            }
+            Console.ReadLine();
+        }
+
+        private static void product2_StockControlEvent()
+        {
+            Console.WriteLine("Stock is low for Phone!");
+        }
+    }
+}
